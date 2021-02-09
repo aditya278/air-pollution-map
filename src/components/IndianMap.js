98893faa93a1ee74f570681b33016cbbbe68a948
aditya278 <aditya.shukla278@gmail.com>
2021-02-10 02:15:57 +0530

    Added states lat and lon and onClick event on each state

--- src/components/IndianMap.js
+++ src/components/IndianMap.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { geoPath, geoAlbers } from "d3-geo";
+import { geoPath, geoConicEqualArea } from "d3-geo";
 import { feature } from "topojson-client";
 
 const IndianMap = ({stateJson, width, height}) => {
   const [geographies, setGeographies] = useState([]);
   
-    const projection = geoAlbers()
+    const projection = geoConicEqualArea()
     .scale(1100)
-    .rotate([-77, 0])
-    .center([12, 26]);;
+    .rotate([-78, 0])
+    .center([12, 26]);
 
   useEffect(() => {
       console.log(stateJson);
       setGeographies(feature(stateJson, stateJson.objects.polygons).features);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleStateClick = stateIndex => {
+      const state = geographies[stateIndex];
+      console.log(`Clicked on state: ${state.properties.st_nm} and Lat: ${state.properties.lat} | Lon: ${state.properties.lon}`);
+  }
 
   return (
     <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
@@ -28,6 +33,7 @@ const IndianMap = ({stateJson, width, height}) => {
                 fill={`rgba(38,50,56,${(1 / geographies.length) * i})`}
                 stroke="red"
                 strokeWidth={0.5}
+                onClick={() => handleStateClick(i)}
             />
             ))
         }
